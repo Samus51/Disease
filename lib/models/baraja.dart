806612502054e44b221277239271d0ba91dbe19f
Samus5 <a_samuel.fernandez.rodriguez@iespablopicasso.es>
2025-01-22 01:58:42 +0100

    Subida de Imgs cartas y demas, (Quedan acciones y mucho más)

---
+++ lib/models/baraja.dart
@@ -0,0 +1,132 @@
+// ignore_for_file: avoid_print
+
+import 'package:disease/models/carta.dart';
+import 'package:disease/models/carta_especial.dart';
+
+class Baraja {
+  final List<Carta> _cartas;
+
+  // Constructor
+  Baraja({required List<Carta> cartas}) : _cartas = cartas;
+
+  // Método para generar el mazo
+  static List<Carta> generarMazo() {
+    List<Carta> mazo = [];
+
+    // Definir los órganos disponibles
+    List<String> organos = ["Corazón", "Cerebro", "Huesos", "Estómago"];
+
+    // Generar cartas de virus y curación para cada órgano
+    for (String organo in organos) {
+      // 4 cartas de virus para cada órgano
+      for (int i = 0; i < 4; i++) {
+        mazo.add(Carta(
+          tipo: TipoCarta.virus,
+          organo: organo,
+          descripcion: "Virus para el $organo",
+        ));
+      }
+
+      // 4 cartas de curación para cada órgano
+      for (int i = 0; i < 4; i++) {
+        mazo.add(Carta(
+          tipo: TipoCarta.curacion,
+          organo: organo,
+          descripcion: "Curación para el $organo",
+        ));
+      }
+
+      // 5 cartas de órgano específicas
+      for (int i = 0; i < 5; i++) {
+        mazo.add(Carta(
+          tipo: TipoCarta.virus,
+          organo: organo,
+          descripcion: "Órgano de $organo",
+        ));
+      }
+    }
+
+    // Generar cartas especiales
+    // Tipo Contagio - 2 cartas
+    for (int i = 0; i < 2; i++) {
+      mazo.add(CartaEspecial(
+        tipoEspecial: TipoEspecial.contagio,
+        descripcion:
+            "Traslada tantos virus como puedas de tus órganos infectados a los órganos de los demás jugadores.",
+      ) as Carta);
+    }
+
+    // Tipo Guante de Látex y Error Médico - 1 carta cada uno
+    mazo.add(CartaEspecial(
+      tipoEspecial: TipoEspecial.guanteLatex,
+      descripcion:
+          'Todos los jugadores oponentes sueltan sus cartas y roban una nueva mano.',
+    ) as Carta);
+    mazo.add(CartaEspecial(
+      tipoEspecial: TipoEspecial.errorMedico,
+      descripcion:
+          'Cambia el cuerpo y mano por completo del jugador por el del oponente.',
+    ) as Carta);
+
+    // Tipo Transplante y Ladrón de Órganos - 3 cartas cada uno
+    for (int i = 0; i < 3; i++) {
+      mazo.add(CartaEspecial(
+        tipoEspecial: TipoEspecial.transplante,
+        descripcion:
+            "Carta especial que cambia un órgano del jugador por otro del oponente.",
+      ) as Carta);
+      mazo.add(CartaEspecial(
+        tipoEspecial: TipoEspecial.ladronDeOrganos,
+        descripcion: "Roba un órgano del oponente.",
+      ) as Carta);
+    }
+
+    // Mezclar el mazo para que las cartas no estén ordenadas
+    mazo.shuffle();
+
+    return mazo;
+  }
+
+  // Getter para acceder a las cartas
+  List<Carta> get cartas => List.unmodifiable(_cartas);
+
+  // Método para contar las cartas por tipo
+  void contarCartas() {
+    int contadorVirus = 0;
+    int contadorCuracion = 0;
+    int contadorEspecial = 0;
+    int contadorOrgano = 0;
+
+    for (var carta in _cartas) {
+      if (carta.tipo == TipoCarta.virus) {
+        contadorVirus++;
+      } else if (carta.tipo == TipoCarta.curacion) {
+        contadorCuracion++;
+        // ignore: unrelated_type_equality_checks
+      } else if (carta.tipo == TipoEspecial.values) {
+        contadorEspecial++;
+      }
+
+      // En caso de que tenga órgano en su tipo
+      // Verificar si la carta tiene un órgano
+      contadorOrgano++;
+    }
+
+    print("Cartas de tipo 'Virus': $contadorVirus");
+    print("Cartas de tipo 'Curación': $contadorCuracion");
+    print("Cartas de tipo 'Especial': $contadorEspecial");
+    print("Cartas de tipo 'Órgano': $contadorOrgano");
+    print("Total de cartas: ${_cartas.length}");
+  }
+}
+
+void main() {
+  // Generar mazo
+  List<Carta> mazo = Baraja.generarMazo();
+
+  // Crear la baraja
+  Baraja baraja = Baraja(cartas: mazo);
+
+  // Contar cartas
+  baraja.contarCartas();
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:disease/main.dart';
+import 'package:disease/mainApp/widgets_cartas.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(PantallaPartidaDisease());
+    await tester.pumpWidget(CartasWidget());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
